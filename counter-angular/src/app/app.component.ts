fc5c135fe3774ae7a2f82ef53de89e59f03e1b6c
Oskar Korczak <oskar.korczak@protonmail.com>
2024-01-30 21:07:20 +0100

    angular counter

---
+++ counter-angular/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  count = 0;
+
+  increment() {
+    this.count++;
+  }
+}
