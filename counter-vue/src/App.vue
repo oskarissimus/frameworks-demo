874553bbb20dedb73ffd8312513b82003f4fc5ce
Oskar Korczak <oskar.korczak@protonmail.com>
2024-01-30 20:56:36 +0100

    Refactor code for improved performance

---
+++ counter-vue/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <button @click="count++">Click me</button>
+  <p>You clicked {{ count }} times.</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    };
+  }
+}
+</script>
